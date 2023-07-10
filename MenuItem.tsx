96f7b7b109d62bc85242ccf33b25e59322e64c15
chandu <chandu.b943@gmail.com>
2023-07-10 10:52:14 -0400

    Add files via upload

--- MenuItem.tsx
+++ MenuItem.tsx
@@ -1,107 +1,82 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route , Link, Routes } from "react-router-dom";
-import { Container, Nav, Navbar, Tab,Row, Col, Dropdown,NavDropdown } from 'react-bootstrap';
+import { Container, Nav,Tabs, Navbar, Tab,Row, Accordion, Card,Col, Dropdown,NavDropdown, Button } from 'react-bootstrap';
+import { CSSTransition } from 'react-transition-group';
+import './menu.css'
 import './menu.css'
 import DropDownSubMenu from './DropDownSubMenu';
 import DropDownMenu from './DropDownMenu';
 import Notfound from './Notfound';
-interface IMenuitemProps {
+
+
+interface MenuItem {
+  title: string;
+  href?: string;
+  submenu?: MenuItem[];
 }
 
-const Menuitem: React.FunctionComponent<IMenuitemProps> = (props) => {
- 
-  return(<>
+const MultiLevelMenu = () => {
+  const menuItems = [
+    {
+      id: 1,
+      title: 'Home',
+      path: '/'
+    },
+    {
+      id: 2,
+      title: 'Products',
+      path: '/products',
+      subItems: [
+        {
+          id: 21,
+          title: 'Electronics',
+          path: '/products/electronics'
+        },
+        {
+          id: 22,
+          title: 'Clothing',
+          path: '/products/clothing'
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: 'Contact',
+      path: '/contact'
+    }
+  ];
+
+  const renderMenuItems = (items:any) => {
+    return items.map((item:any) => {
+      if (item.subItems) {
+        return (
+          <NavDropdown title={item.title} key={item.id}>
+            {renderMenuItems(item.subItems)}
+          </NavDropdown>
+        );
+      } else {
+        return (
+          <Nav.Item key={item.id}>
+             <Nav.Link as={Link}  to={item.path}>{item.title}</Nav.Link>
+          </Nav.Item>
+        );
+      }
+    });
+  };
+
+  return (
     <Router>
-  <Navbar className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-      <Row>
-          <Col> 
-              <Nav variant="tabs" id="justify-tab-example"  className="main-menu" justify defaultActiveKey="/home">
-                <Nav.Item>
-                  <Nav.Link  as={Link} to="/MyPreference" eventKey="first">My Preference</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link as={Link} to="/MonthlyInitialization" eventKey="second"> Monthly Initialization</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                  <Nav.Link eventKey="third"> Control Total Workyears</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="fouth"> Monthly Processing</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                  <Nav.Link eventKey="fifth">Reports</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="sixth"> Yearly Initialization</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                  <Nav.Link eventKey="seven">Cost Analysis</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                  <Nav.Link eventKey="eight">Maintenance</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                  <Nav.Link eventKey="nine">Administration</Nav.Link>
-              </Nav.Item>
-              </Nav>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-          <Tab.Content>
-            <Tab.Pane eventKey="first">
-              <div className='submenu'>
-                <Nav variant="tabs">
-                <Nav.Item>
-                <Nav.Link as={Link} to="/MyPreference/option1" eventKey="infirst">option 1</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                <Nav.Link as={Link} to="/MyPreference/option2" eventKey="insecond">Option 2</Nav.Link>
-                </Nav.Item>
-                </Nav>
-              </div>
-            </Tab.Pane>
-            <Tab.Pane eventKey="second"> 
-              <div className='submenu'>
-                <Nav variant="tabs">
-                <Nav.Item>
-                <Nav.Link as={Link} to="/MonthlyInitialization/option1" eventKey="insecondfirst">option 1</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                <Nav.Link as={Link} to="/MonthlyInitialization/option2" eventKey="insecond">Option 2</Nav.Link>
-                </Nav.Item>
-                </Nav>
-              </div>
-            </Tab.Pane>
-          </Tab.Content>
-          </Col>
-      </Row>
-    </Tab.Container>
+      <Nav>
+        {renderMenuItems(menuItems)}
+      </Nav>
     <Routes>
-        <Route path="/MyPreference"  />
-        <Route path="/MyPreference/option1" Component={DropDownSubMenu} />
-        <Route path="/MyPreference/option2" Component={DropDownSubMenu} />
-        
-    </Routes>
-    <Routes>
-        <Route path="/MonthlyInitialization" Component={DropDownMenu} />
-        <Route path="/MonthlyInitialization/option1" Component={DropDownMenu} />
-        <Route path="/MonthlyInitialization/option2" Component={DropDownMenu} />
-    </Routes>
-   
+      <Route path="/"  Component={DropDownSubMenu} />
+      <Route path="/products"Component={DropDownSubMenu} />
+      <Route path="/products/electronics" Component={DropDownSubMenu} />
+      <Route path="/products/clothing"Component={DropDownSubMenu} />
+      </Routes>
     </Router>
-  </>) ;
+  );
 };
 
-export default Menuitem;
+export default MultiLevelMenu;
