f3c58d2888a711e85fce8f2bc3f5da25da37ec49
chandu <chandu.b943@gmail.com>
2023-07-13 09:39:51 -0400

    new repo for bala

---
+++ menuItems.ts
@@ -0,0 +1,35 @@
+
+export const menuItems = [
+  {
+    title: 'Services',
+    url: '/services',
+    submenu: [
+      {
+        title: 'web design',
+        url: 'web-design',
+      },
+      {
+        title: 'web development',
+        url: 'web-dev',
+      },
+      {
+        title: 'SEO',
+        url: 'seo',
+      },
+    ],
+  },
+  {
+    title: 'About',
+    url: '/about',
+    submenu: [
+      {
+        title: 'Who we are',
+        url: 'who-we-are',
+      },
+      {
+        title: 'Our values',
+        url: 'our-values',
+      },
+    ],
+  },
+];
